6303839ecfa1930686f470fcb19039a26f67a929
jiwon <kjw042626@gmail.com>
2025-04-18 21:31:59 +0900

    데이터베이스 스키마 자동생성(1)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,66 +15,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            /* INSERT
+
             Member member = new Member();
             member.setId(2L);
-            member.setName("HelloB");
+            member.setUsername("B");
+            member.setRoleType(RoleType.ADMIN);
+
             em.persist(member);
-             */
-
-            /* FIND
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
-             */
-
-
-            /* UPDATE
-            findMember.setName("HelloJPA");
-             */
-
-
-            /* DELETE
-            em.remove(findMember);
-             */
-
-            /* SELECT 쿼리문 조회
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
-             */
-
-            /* INSERT
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
-            // 영속(DB에 바로 쿼리가 날라가지 않음)
-            // 1차 캐시에 저장됨
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
-             */
-
-            /* 1차 캐시에서 꺼내온 다는 것을 알 수 있다.
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
-            System.out.println("findMember.id = " + findMember1.getId());
-            System.out.println("findMember.name = " + findMember1.getName());
-
-            System.out.println("result = " + (findMember1 == findMember2));
-             */
-
-            Member findMember = em.find(Member.class, 150L);
-            findMember.setName("Z");
-
-            System.out.println(" ======================= ");
 
             tx.commit();
         } catch (Exception e) {
