fa6dc0c4289200d15f84b413698de0d1683e7dd4
jiwon <kjw042626@gmail.com>
2025-04-23 19:04:08 +0900

    기본키 맵핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,13 +14,24 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("A");
-            String username = member.getUsername();
-            System.out.println("username = " + username);
+            Member member1 = new Member();
+            member1.setUsername("A");
+            Member member2 = new Member();
+            member2.setUsername("B");
+            Member member3 = new Member();
+            member3.setUsername("C");
 
+            System.out.println("====================");
 
-            em.persist(member);
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("member1.getId() = " + member1.getId());
+            System.out.println("member2.getId() = " + member2.getId());
+            System.out.println("member3.getId() = " + member3.getId());
+
+            System.out.println("====================");
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,10 +3,15 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+initialValue = 1, allocationSize = 50)
 public class Member {
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name", nullable = false)
