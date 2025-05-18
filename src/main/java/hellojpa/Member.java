2ebb2abebd0352cf92254c8aa03991fcb52abc24
jiwon <kjw042626@gmail.com>
2025-05-18 21:46:03 +0900

    일대다 [1:N]

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -13,7 +13,7 @@ public class Member {
     private String username;
 
     @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
 
     public Long getId() {
