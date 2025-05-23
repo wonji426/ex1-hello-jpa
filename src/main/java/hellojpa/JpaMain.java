164827727b7232227e28b2fe8fe3d10626d8e503
jiwon <kjw042626@gmail.com>
2025-05-23 18:19:34 +0900

    즉시 로딩과 지연 로딩

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,8 @@ package hellojpa;
 import jakarta.persistence.*;
 import org.hibernate.Hibernate;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -15,20 +17,35 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Team team2 = new Team();
+            team.setName("TeamB");
+            em.persist(team2);
+
             Member member1 = new Member();
-            member1.setUsername("Jack1");
+            member1.setUsername("member1");
+            member1.ChangeTeam(team);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.ChangeTeam(team2);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass()); //Proxy
-            Hibernate.initialize(refMember); //강제 초기화
+//            Member m = em.find(Member.class, member1.getId());
+
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class).getResultList();
 
             tx.commit(); //커밋시 SQL문 나감
         } catch (Exception e) {
             tx.rollback();//에러 시 롤백
+            e.printStackTrace();
         } finally {
             em.close();//닫기
         }
